71ca055be23a43e6f6048e3774821036cd8434b6
jjyangjian <jjyangjian@qq.com>
2019-07-09 16:02:35 +0800

    fuck

---
+++ projectJava/app/src/main/java/com/fuck/yangjian/projectjava/JJMainActivity.java
@@ -0,0 +1,17 @@
+package com.fuck.yangjian.projectjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JJMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jjmain);
+
+        //fuck????fdsfdsafa
+
+
+    }
+}
